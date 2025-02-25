25775d159118e7c7bbef87044ec55fa1b735a780
Yanrs16 <yanrodrigues973@gmail.com>
2025-02-25 17:06:38 -0300

    fotter.php

--- restaurante/html/fotter.php
+++ restaurante/html/fotter.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="paginainicial.css">
-    <title>Document</title>
+    <link rel="shortcut icon" href="https://i.imgur.com/o4hZ68k.png" type="image/x-icon"> <!-- Link direto para o ícone -->
+    <title>Restaurante XP dos Sabores</title>
 </head>
 <body>
-    <!-- Rodapé -->
 <footer class="rodape">
-        <p>&copy; 2025 Pizzaria Alviverde. Todos os direitos reservados.</p>
+        <div>
+            <a class="box" href="index4.html">Localização</a>
+            <a class="box" href="avaliacoes.html">Avaliações</a>
+        </div>
+        <p>&copy; 2025 Restaurante XP dos Sabores. Todos os direitos reservados.</p>
     </footer>
-</body>
-</html>
 
