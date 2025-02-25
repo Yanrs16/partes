795ec787f4881f80f61606ea5598274bf84ff794
Yanrs16 <yanrodrigues973@gmail.com>
2025-02-25 17:06:07 -0300

    content.php

--- restaurante/html/content.php
+++ restaurante/html/content.php
@@ -1,30 +1,30 @@
- <!DOCTYPE html>
- <html lang="en">
- <head>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="paginainicial.css">
-    <title>Document</title>
- </head>
- <body>
-    <!-- Meio -->
- <main class="meio">
+    <link rel="shortcut icon" href="https://i.imgur.com/o4hZ68k.png" type="image/x-icon"> <!-- Link direto para o ícone -->
+    <title>Restaurante XP dos Sabores</title>
+</head>
+<main class="content">
         <div class="links">
             <a class="box" href="index.html">Inicio</a>
-            <a class="box" href="index2.html">Pizzas</a>
-            <a class="box" href="index3.html">Bebidas</a>
-            <a class="box" href="index4.html">Localização</a>
+            <a class="box" href="index2.html">Cardápio</a>
+            <a class="box" href="promocoes.html">Promoções</a>
+            <a class="box" href="pedidos.html">Pedidos </a>
+            <a class="box" href="area-do-cliente.html">Área do Cliente</a>
         </div>
 
         <hr>
 
         <section class="boas-vindas">
-            <h1>Bem-vindo à Pizzaria Alviverde</h1>
-            <h2>Onde o sabor é sempre vencedor! Nossas pizzas são feitas com os melhores ingredientes e uma massa especial, criando uma experiência única em cada fatia. Venha conferir o sabor que vai conquistar você!</h2>
+            <h1>Bem-vindo ao Restaurante XP dos Sabores</h1>
+            <h2>Uma jornada gastronômica inesquecível! Pratos preparados com ingredientes selecionados e um toque especial para transformar cada refeição em uma experiência única!</h2>
         </section>
     </main>
 
     <hr>
- </body>
- </html>
+</body>
+</html>
